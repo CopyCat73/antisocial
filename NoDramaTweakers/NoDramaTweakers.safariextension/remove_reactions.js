832277e725fefe910b7b3e8a2a3c233ce1132e3d
Nick <copycat73@gmail.com>
2018-07-24 12:49:02 +0200

    Initial commit

---
+++ NoDramaTweakers/NoDramaTweakers.safariextension/remove_reactions.js
@@ -0,0 +1,5 @@
+// create a para and insert it at the top of the body
+$('.replies').empty();
+$('#commentColumn').empty();
+$('.commentCount').parent().empty();
+$('.notification:contains("blokkeert")').remove();
